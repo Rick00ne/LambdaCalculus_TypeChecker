d0266d3f3c37d2bb29cb0755b76f44078bbff373
Richard Jurko <richard.jurko.2@student.tuke.sk>
2024-03-27 20:12:49 +0100

    fake report submitting (test)

--- report_form.php
+++ report_form.php
@@ -9,18 +9,11 @@
 	   }
 
 	   if (empty($errors)) {
-	       $email = 'NoReply@LamCalTypeCheck.com';
-	       $toEmail = 'richard.jurko.2@student.tuke.sk';
-	       $emailSubject = 'LamCalTypeCheck Bug Report';
-	       $headers = ['From'=>$email, 'Content-type'=>'text/html; charset=utf-8'];
-	       $bodyParagraphs = [ 'Report message:', $message];
-	       $body = join(PHP_EOL, $bodyParagraphs);
-
-	       if (mail($toEmail, $emailSubject, $body, $headers)) {
-                   echo json_encode(['Message'=>'Thank you for your feedback!']);
+	       if (true) {
+             echo json_encode(['Message'=>'Thank you for your feedback!']);
 	       }
 	       else {
-	           header("HTTP/1.0 500 Internal Server Error");
+	         header("HTTP/1.0 500 Internal Server Error");
                    echo error_get_last();
 	       }
 
